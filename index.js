4fafca0576e1a8eef5b073ede257a83a44d7e4a4
VMR21 <reddymodith@gmail.com>
2025-06-28 10:57:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 const app = express();
 const PORT = process.env.PORT || 3000;
 const SELF_URL = "https://stiefundata.onrender.com/leaderboard/top14";
-const API_KEY = "fOm9JaRhjaSGiJDI9UhSJUtd3KRlBe8c";
+const API_KEY = "EKi0S6sosGRS9EDXUeoI4b7vDFX1NiSB";
 
 let cachedData = [];
 
